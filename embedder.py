be26f8b44d5b0bda79c52c96318a3cd8dd6d5d6f
Kieran <hawkinski2019@gmail.com>
2026-04-26 22:57:18 -0700

    fix: repeat third experiment without context

--- embedder.py
+++ embedder.py
@@ -1,6 +1,6 @@
 def get_embeddings_batch(texts, openai_client):
     response = openai_client.embeddings.create(
-        input=texts, model="text-embedding-3-small"
+        input=texts, model="text-embedding-3-large"
     )
     embeddings = [item.embedding for item in response.data]
     return embeddings
